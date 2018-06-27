37067a8c65469a261f6a201adbdf613d6535396e
aleksey-slavik <aleksey.slavik@yandex.ru>
2018-06-27 10:19:00 +0300

    initial commit

---
+++ src/main/java/ua/edu/uipa/math/MathApiApplication.java
@@ -0,0 +1,12 @@
+package ua.edu.uipa.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathApiApplication.class, args);
+    }
+}
