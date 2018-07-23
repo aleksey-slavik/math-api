5ab5edfcf602132db6f6f1238fe51578ad76474f
oleksiislavik <oleksii.slavik@sophos.com>
2018-07-23 19:57:15 +0300

    update question api

--- src/main/java/ua/edu/uipa/math/controller/QuestionController.java
+++ src/main/java/ua/edu/uipa/math/controller/QuestionController.java
@@ -1,7 +1,6 @@
 package ua.edu.uipa.math.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -21,18 +20,13 @@ public class QuestionController {
         this.questionRepo = questionRepo;
     }
 
-    @RequestMapping(
-            value = "/{id}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping("/{id}")
     ResponseEntity<?> getQuestionById(@PathVariable Long id) {
         Optional<Question> question = questionRepo.findById(id);
         return ResponseEntity.ok().body(question);
     }
 
-    @RequestMapping(
-            method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping
     ResponseEntity<?> postQuestion(@Validated @RequestBody Question question) {
         question.timestamp();
         Question response = questionRepo.save(question);
